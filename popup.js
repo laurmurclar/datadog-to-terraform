3b07f3ee22cf54c1665b17d9ab84e1046822256f
Laura Murphy-Clarkin <laura@intercom.io>
2018-08-09 11:42:46 +0100

    Remove unnecesary comments

--- popup.js
+++ popup.js
@@ -1,28 +1,18 @@
 import { generateTerraformCode } from './generator.js';
 
 function onClick() {
-  // get the input
   var resourceName = document.getElementById('resourceName').value;
   var datadogJson = document.getElementById('datadogJson').value;
 
-  // convert it
   var terraformAlarmCode = generateTerraformCode(resourceName, JSON.parse(datadogJson));
-
-  // add to the result input
   document.getElementById('result').innerHTML = terraformAlarmCode;
 
-  // copy to clipboard
   copyResult();
 }
 
 function copyResult() {
-  /* Get the text field */
   var copyText = document.getElementById('result');
-
-  /* Select the text field */
   copyText.select();
-
-  /* Copy the text inside the text field */
   document.execCommand('copy');
 }
 

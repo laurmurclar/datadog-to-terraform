5556ce45d53d2973d5b3a010fdd802df8f372f85
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2019-09-18 16:25:08 +0100

    Add error messages and style improvements

--- popup.js
+++ popup.js
@@ -4,19 +4,40 @@ function onClick() {
   var resourceName = document.getElementById('resourceName').value;
   var datadogJson = document.getElementById('datadogJson').value;
 
-  var terraformAlarmCode = generateTerraformCode(resourceName, JSON.parse(datadogJson));
-  document.getElementById('result').innerHTML = terraformAlarmCode;
-
-  document.getElementById('outputWrapper').classList.add('active');
-  document.getElementById('onClickMessage').innerHTML = 'Copied to clipboard!';
-
-  copyResult();
+  try {
+    var terraformAlarmCode = generateTerraformCode(resourceName, JSON.parse(datadogJson));
+    addDomElementsForResult(terraformAlarmCode);
+    copyResultToClipboard();
+    updateStatusMessage('Copied to clipboard!', false);
+  } catch (e) {
+    updateStatusMessage(e, true);
+  }
 }
 
-function copyResult() {
+function addDomElementsForResult(terraformAlarmCode) {
+  var resultTextArea = document.createElement('textarea');
+  resultTextArea.id = 'result';
+  resultTextArea.innerHTML = terraformAlarmCode;
+
+  var outputWrapperDiv = document.getElementById('outputWrapper');
+  outputWrapperDiv.appendChild(resultTextArea);
+  outputWrapperDiv.classList.add('active');
+}
+
+function copyResultToClipboard() {
   var copyText = document.getElementById('result');
   copyText.select();
   document.execCommand('copy');
+}
+
+function updateStatusMessage(message, isError) {
+  var newMessageText = isError ? '❗️' + message : '🎉' + message;
+  var statusMessageElement = document.getElementById('statusMessage');
+  statusMessageElement.innerHTML = newMessageText;
+  document.getElementById('convertButton').style.marginBottom = '0';
+  if (isError) {
+    statusMessageElement.style.color = 'red';
+  }
 }
 
 document.getElementById('convertButton').addEventListener('click', onClick);

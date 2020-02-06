49c8c18dfa8f1dc9675a68319914e8070f7ae761
Flavius.Cater <flavius.cater@3pillarglobal.com>
2020-02-06 12:52:40 +0200

    Added dashboard generator

--- popup.js
+++ popup.js
@@ -1,14 +1,25 @@
-import { generateTerraformCode } from "./generator.js";
+import { generateTerraformCode } from "./monitor_generator.js";
+import {generateDashboardTerraformCode} from "./dashboard_generator.js";
 
 function onClick() {
   var resourceName = document.getElementById("resourceName").value;
   var datadogJson = document.getElementById("datadogJson").value;
 
   try {
-    var terraformAlarmCode = generateTerraformCode(
-      resourceName,
-      JSON.parse(datadogJson)
-    );
+      var terraformAlarmCode;
+      if (document.getElementById('monitorChoice').checked) {
+          //rate_value = document.getElementById('r1').value;
+          terraformAlarmCode = generateTerraformCode(
+              resourceName,
+              JSON.parse(datadogJson)
+          );
+      } else {
+          terraformAlarmCode = generateDashboardTerraformCode(
+              resourceName,
+              JSON.parse(datadogJson)
+          );
+      }
+
     addDomElementsForResult(terraformAlarmCode);
     copyResultToClipboard();
     updateStatusMessage("Copied to clipboard!", false);

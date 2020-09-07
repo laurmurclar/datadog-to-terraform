f88521fa25c6a80029d66fd276e6bf9ead9b1b08
Laura Murphy-Clarkin <laurmurclar@users.noreply.github.com>
2020-09-07 13:22:26 +0100

    Add example files (#29)

---
+++ examples/monitor.tf
@@ -0,0 +1,26 @@
+resource "datadog_monitor" "example_monitor" {
+name = "CPU load is high"
+type = "metric alert"
+query = "avg(last_5m):avg:system.cpu.system{*} > 50"
+message = <<EOF
+{{#is_alert}}
+ Possible explanations are x, y or z. Check blah, blah blah.
+{{/is_alert}} 
+
+{{#is_recovery}}
+CPU is at an acceptable level again
+{{/is_recovery}}  @all
+EOF
+tags = ["team-cpu"]
+notify_audit = false
+locked = true
+timeout_h = 0
+new_host_delay = 300
+require_full_window = false
+notify_no_data = false
+renotify_interval = "0"
+escalation_message = ""
+include_tags = true
+thresholds {
+critical = 50
+}}

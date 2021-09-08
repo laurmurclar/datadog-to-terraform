9988ac75dad95980a5ecf58994200868c70ace5b
Frederic G. MARAND <fgm@osinet.fr>
2021-09-08 11:58:00 +0200

    Issue #61: Upgrade dependencies, fix convert script.

--- app/cli.js
+++ app/cli.js
@@ -2,7 +2,18 @@ import fs from "fs";
 import { generateTerraformCode } from "./monitor-converter";
 import { generateDashboardTerraformCode } from "./dashboard-converter";
 
-const json = fs.readFileSync("/dev/stdin").toString();
+let json;
+switch (process.argv.length) {
+  case 2:
+    json = fs.readFileSync("/dev/stdin").toString();
+    break;
+  case 3:
+    json = fs.readFileSync(process.argv[2]).toString();
+    break;
+  default:
+    console.error("Usage: \nyarn convert < somefile.json\nor\nyarn convert somefile.json");
+    process.exit(1);
+}
 const parsedJson = JSON.parse(json);
 const resourceName = "replaceMeNow";
 
